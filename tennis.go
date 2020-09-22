903b7885238c3d29f817e83722141a19efd2e703
NolionsChen <nolionschen@jetfuel.com.tw>
2020-09-22 10:32:26 +0800

    補完Result - LoveAll code

---
+++ tennis.go
@@ -0,0 +1,24 @@
+package main
+
+type Game struct {
+	Player1      string
+	Player2      string
+	Player1Score int
+	Player2Score int
+}
+
+func TennisGame(player1, player2 string) *Game {
+	game := &Game{
+		Player1:      player1,
+		Player2:      player2,
+		Player1Score: 0,
+		Player2Score: 0,
+	}
+
+	return game
+}
+
+func (g *Game) Result() string {
+	result := "LoveAll"
+	return result
+}
