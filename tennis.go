14e5c3dedf2463d73e447e4a9c40352387f64a3c
NolionsChen <nolionschen@jetfuel.com.tw>
2020-09-22 11:40:11 +0800

    refactor tennis result method

--- tennis.go
+++ tennis.go
@@ -1,10 +1,20 @@
 package main
 
+var scoreCodes map[int]string
+
 type Game struct {
 	Player1      string
 	Player2      string
 	Player1Score int
 	Player2Score int
+}
+
+func init()  {
+	scoreCodes = make(map[int]string)
+	scoreCodes[0] = "Love"
+	scoreCodes[1] = "Fifteen"
+	scoreCodes[2] = "Thirty"
+	scoreCodes[3] = "Forty"
 }
 
 func TennisGame(player1, player2 string) *Game {
@@ -21,12 +31,8 @@ func TennisGame(player1, player2 string) *Game {
 func (g *Game) Result() string {
 	result := "Love - All"
 
-	if g.Player1Score == 1 {
-		result = "Fifteen - Love"
-	} else if g.Player1Score == 2 {
-		result = "Thirty - Love"
-	} else if g.Player1Score == 3 {
-		result = "Forty - Love"
+	if g.Player1Score > 0 {
+		result = scoreCodes[g.Player1Score] + " - Love"
 	}
 
 	return result
