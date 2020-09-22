23617daef4b314b9eadcb24e7d002e60b6262512
NolionsChen <nolionschen@jetfuel.com.tw>
2020-09-22 10:29:59 +0800

    First LoveAll Test Case

---
+++ tennis_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoveAll(t *testing.T) {
+	game := TennisGame("A", "B")
+	result := game.Result()
+	assert.Equal(t, "LoveAll", result)
+}
