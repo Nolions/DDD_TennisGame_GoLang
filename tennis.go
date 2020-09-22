3eca2ce66b75869655cc99d05b2acd35d24d2e51
NolionsChen <nolionschen@jetfuel.com.tw>
2020-09-22 12:11:04 +0800

    Deuce 情境判斷

--- tennis.go
+++ tennis.go
@@ -32,7 +32,12 @@ func (g *Game) Result() string {
 	result := "Love - All"
 
 	if g.Player1Score == g.Player2Score {
-		result = scoreCodes[g.Player1Score] + " - All"
+		if g.Player1Score >= 3 {
+			result = "Deuce"
+		} else {
+			result = scoreCodes[g.Player1Score] + " All"
+		}
+
 	} else if g.Player1Score > 0 || g.Player2Score > 0 {
 		result = scoreCodes[g.Player1Score] + " - " + scoreCodes[g.Player2Score]
 	}
