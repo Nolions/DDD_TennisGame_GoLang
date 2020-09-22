c0c214a83072084cb9bab05b3f85d741bf9c210e
NolionsChen <nolionschen@jetfuel.com.tw>
2020-09-22 11:52:38 +0800

    新增 Fifteen - All 判斷

--- tennis.go
+++ tennis.go
@@ -31,7 +31,9 @@ func TennisGame(player1, player2 string) *Game {
 func (g *Game) Result() string {
 	result := "Love - All"
 
-	if g.Player1Score > 0 || g.Player2Score > 0 {
+	if g.Player1Score == g.Player2Score {
+		result = scoreCodes[g.Player1Score] + " - All"
+	} else if g.Player1Score > 0 || g.Player2Score > 0 {
 		result = scoreCodes[g.Player1Score] + " - " + scoreCodes[g.Player2Score]
 	}
 
